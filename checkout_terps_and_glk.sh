19a0e9740ac70922510b09747d88469bb78b10e4
Stephen Gutknecht <stephen.gutknecht@gmail.com>
2017-08-21 10:44:49 -0400

    bash script for checkout now checks errors and does basic sanity checks on path

--- checkout_terps_and_glk.sh
+++ checkout_terps_and_glk.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
 
 #
+# (c) Copyright 2017 Stephen A. Gutknecht. All Rights Reserved.
+# License granted: Public domain, world wide.
+#
+
+#
 # Makes assumption that this is virgin run, no previous checkout files.
 #
 
 # starting from current path, root of this project.
+pwd
+if [ ! -d "src/main/jni/makefiles" ]; then
+  echo "!!!! ERROR_CODE:A00 expected folder does not exist, aborting"
+  exit 1
+fi
+
+mkdir -p src/main/jni/loadable_engines
 cd src/main/jni/loadable_engines
 
 pwd
 
-git clone https://github.com/erkyrath/remglk remglk
+# function for git source control checkout
+# arguments: 1: network path to download from 2: destination directory 3: git commit reference
+gitsourcecheckout () {
+   echo "git source checkout, Parameter #1 is $1 Parameter #2 is $2"
+   if [ -d "$2" ]; then
+      echo "WARNING: Destination folder already exists before git source checkout"
+   fi
+
+   git clone $1 $2
+
+   # check error returned from git
+   if [ $? -gt 0 ]; then
+      echo "!!!! ERROR_CODE:B00 git source checkout ERROR, aborting."
+      exit 2
+   fi
+}
+
+
+gitsourcecheckout https://github.com/erkyrath/remglk remglk
 cp ../makefiles/remglk/Android.mk remglk/
 
-git clone https://github.com/erkyrath/glulxe glulxe
+gitsourcecheckout https://github.com/erkyrath/glulxe glulxe
 cp ../makefiles/glulxe/Android.mk glulxe/
